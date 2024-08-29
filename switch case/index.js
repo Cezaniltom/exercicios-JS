70cf07367b38beb02c60221da4c03c48a5a8ec8a
cezaniltom <cezaniltom@outlook.com>
2024-08-29 09:29:19 -0300

    Exercicio 1 Switch case finalizado

--- switch case/index.js
+++ switch case/index.js
@@ -1,7 +1,33 @@
 // Exercício Fácil
 // Escreva um programa em JavaScript que recebe uma cor em inglês e retorna o seu nome em português. As cores podem ser: "red" (vermelho), "blue" (azul), "green" (verde), "yellow" (amarelo) e "black" (preto).
 
+const color = 'black'
 
+switch (color) {
+    case 'red':
+        console.log('Vermelho')
+        break;
+    
+    case 'blue':
+        console.log('Azul')
+        break;
+    
+    case 'green':
+        console.log('Verde')
+        break;
+
+    case 'yellow':
+        console.log('Amarelo')
+        break;
+
+    case 'black':
+        console.log('Preto')
+        break;
+
+    default:
+        console.log('Essa cor não é válida')
+        break;
+}
 
 
 
