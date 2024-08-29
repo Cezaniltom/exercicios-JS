0821fed89259901930897cc080b7688343868ae7
Cezaniltom <cezaniltom@outlook.com>
2024-08-28 22:13:37 -0300

    Adicionando exercicios do if else

---
+++ if - else/index.js
@@ -0,0 +1,65 @@
+// Exercício Fácil:
+// Escreva um programa em JavaScript que verifica se um número é positivo, negativo ou igual a zero.
+
+// const positivoNegativo = -1
+
+// if(positivoNegativo > 0 ) {
+//     console.log('O número é positivo')
+// } else if (positivoNegativo < 0) {
+//     console.log('O número é negativo')
+// } else {
+//     console.log('O número é igual a zero')
+// }
+
+
+
+
+// Exercício Moderado:
+// Escreva um programa em JavaScript que verifica se um número é par ou ímpar.
+
+
+// const parImpar = 5
+
+// if(parImpar % 2 == 0) {
+//     console.log('O número é par')
+// } else {
+//     console.log('O número é impar')
+// }
+
+
+
+
+// Exercício Intermediário:
+// Escreva um programa em JavaScript que verifica se um número é divisível por 3 e por 5 ao mesmo tempo
+
+
+// const divisivel = 32
+
+// if(divisivel % 3 == 0 && divisivel % 5 == 0 ) {
+//     console.log('O número é divisivel')
+// } else {
+//     console.log('O número não é divisivel')
+// }
+
+
+
+// Exercício Avançado:
+// Escreva um programa em JavaScript que verifica se um ano é bissexto.
+// Dica: um ano bissexto é divisível por 4, mas não por 100, a menos que também seja divisível por 400.
+
+
+// const bissexto = 2018
+
+// if(bissexto % 4 == 0 && bissexto % 100 != 0 ) {
+//     console.log('O número é divisivel')
+// } else {
+//     console.log('O número não é divisivel')
+// }
+
+
+
+
+// Exercício Desafiador
+// Escreva um programa em JavaScript que verifica se um número é um *palíndromo*.
+
+// Obs: Um *palíndromo* é um número que permanece o mesmo quando seus dígitos
