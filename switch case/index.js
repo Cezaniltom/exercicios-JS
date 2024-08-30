d218d1445569b6f7b12bdd228a50fe65f35e5e08
cezaniltom <cezaniltom@outlook.com>
2024-08-30 14:18:16 -0300

    Exercicio 4 Switch case finalizado

--- switch case/index.js
+++ switch case/index.js
@@ -137,8 +137,22 @@
 // **Exemplo de uso:** `calculadora(5, 3, "somar")` deve retornar 8.
 
 
+function calculadora(num1, num2, operacao) {
+    switch (operacao) {
+        case "somar":
+            return num1 + num2;
+        case "subtrair":
+            return num1 - num2;
+        case "multiplicar":
+            return num1 * num2;
+        case "dividir":
+            return num1 / num2;
+        default:
+            return "Operação inválida.";
+    }
+}
 
-
+console.log(calculadora(2, 1, "dividir"));
 
 
 // Exercício Desafiador
