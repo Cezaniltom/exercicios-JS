7b8965e4e4ca575d1800c52b7cc1bd5e600f56a5
cezaniltom <cezaniltom@outlook.com>
2024-08-31 11:06:25 -0300

    Exercicio 1 Do while finalizado

---
+++ Do - while/index.js
@@ -0,0 +1,11 @@
+// Exercício Fácil:
+// Escreva um programa em JavaScript que imprima todos os números de 1 a 10 utilizando um loop while.
+
+
+// let numeros = 1;
+
+// do {
+//     console.log(numeros)
+//     numeros++
+// } while (numeros <= 10)
+
