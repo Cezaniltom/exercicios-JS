169743c9113e39d7194dae92ab274c84b266b8d6
cezaniltom <cezaniltom@outlook.com>
2024-09-02 16:00:30 -0300

    Exercicio 3 Do while finalizado

--- Do - while/index.js
+++ Do - while/index.js
@@ -15,11 +15,39 @@
 // Escreva um programa em JavaScript que calcule a soma dos números de 1 a 100 utilizando um loop while. Imprima o resultado no final.
 
 
-let soma = 0;
-let i = 1;
+// let soma = 0;
+// let i = 1;
 
-while (i < 100) {
-    soma += i;
-    console.log(`a soma de ${i} + ${soma} é, ${soma + i}`);
-    i++;
-}; 
+// while (i < 100) {
+//     soma += i;
+//     console.log(`a soma de ${i} + ${soma} é, ${soma + i}`);
+//     i++;
+// }; 
+
+
+
+
+// Exercício Intermediário:
+// Escreva um programa em JavaScript que encontre e imprima todos os números primos de 1 a 100 utilizando um loop while.
+
+
+// let numero = 2;
+
+// while (numero <= 100) {
+//     let divisor = 2;
+//     let Primo = true; 
+   
+//     while (divisor < numero) {
+//         if (numero % divisor === 0) {
+//             Primo = false;
+//             break;
+//         }
+//         divisor++;
+//     }
+
+//     if (Primo) {
+//         console.log(numero);
+//     }
+
+//     numero++;
+// }
