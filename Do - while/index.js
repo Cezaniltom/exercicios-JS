df6a4efbf0b8a8dcfb9b8828acb87315b54a02fc
cezaniltom <cezaniltom@outlook.com>
2024-09-01 22:55:51 -0300

    Exercicio 2 Do while

--- Do - while/index.js
+++ Do - while/index.js
@@ -9,3 +9,17 @@
 //     numeros++
 // } while (numeros <= 10)
 
+
+
+// Exercício Moderado:
+// Escreva um programa em JavaScript que calcule a soma dos números de 1 a 100 utilizando um loop while. Imprima o resultado no final.
+
+
+let soma = 0;   // Variável para armazenar a soma
+let i = 1;      // Variável para o contador
+
+do {
+    soma += i;  // Adiciona o valor de i à soma
+    i++;        // Incrementa o contador
+    console.log(i, soma);  // Imprime o resultado
+} while (i < 100);  // Continua enquanto i for menor ou igual a 100
